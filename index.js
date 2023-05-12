87058a33ffd2b4ea25b2b3a16e6be5ab9a8285a6
jestin-kuriakose <jestin@teslaelectronics.ca>
2023-05-12 16:35:56 +0400

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/v1', require("./routes/root"))
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
 app.listen(process.env.PORT || 3000)
