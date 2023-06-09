ebc021453a952a223add10c876d114e15526c522
jestin-kuriakose <jestin@teslaelectronics.ca>
2023-06-10 02:09:22 +0400

    fixed aws region

--- config/s3Connect.js
+++ config/s3Connect.js
@@ -7,7 +7,7 @@ const s3 = new S3Client({
         accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
     },
-    region: process.env.AWS_REGION
+    region: process.env.AWS_S3_REGION
 });
 
 const s3Connect = async (file) => {
