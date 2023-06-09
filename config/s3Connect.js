14557c5a641a1140cbd78f7987b125f21d9c1746
jestin-kuriakose <jestin@teslaelectronics.ca>
2023-06-10 01:48:42 +0400

    changed env variable names for AWS

--- config/s3Connect.js
+++ config/s3Connect.js
@@ -4,8 +4,8 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 // Connecting to AWS S3
 const s3 = new S3Client({
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
     },
     region: process.env.AWS_REGION
 });
